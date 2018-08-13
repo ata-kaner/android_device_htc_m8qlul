d03e29df6f7872cc3e3c3b38efc1473d8c6a333a
brinlyau <uberlaggydarwin@gmail.com>
2018-08-14 00:16:55 +0200

    m8qlul: remove languages-full.mk
     * full_base_telephony.mk already includes language pack
    
    Change-Id: I0e28542250d6cfe80b6649add486876780754ca3

--- device.mk
+++ device.mk
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit the vendor files
