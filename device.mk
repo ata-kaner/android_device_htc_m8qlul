75a93941b37b769e4d3e81ad2acf074bc8a70cd0
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    m8qlul: Adding Audio HAL V5: Direction API
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- device.mk
+++ device.mk
@@ -81,10 +81,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio.effect@5.0-impl \
     audio.a2dp.default \
     audio.primary.msm8916 \
     audio_amplifier.msm8916 \
