0f2bf11e918e1c34633931df95b3307b128513a0
Julian Veit <Claymore1298@gmail.com>
2021-08-17 13:33:28 +0200

    m8qlul: init: add more product info for safetynet
    Idea taken from:
    https://gerrit.aicp-rom.com/c/AICP/device_motorola_lux/+/110008
    
    so credits go to Ali B.
    
    Change-Id: Ic0094fcefcdcea8fc4595307dd25b35f62830bf0

--- aicp_m8qlul.mk
+++ aicp_m8qlul.mk
@@ -23,14 +23,18 @@ PRODUCT_MODEL := HTC One M8s
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-     DEVICE_MAINTAINERS="Chris Papageorgiou (root-expert)" \
-     PRODUCT_NAME="HTC One M8s" \
-     PRODUCT_DEVICE="m8qlul"
+     DEVICE_MAINTAINERS="Chris Papageorgiou (root-expert), Atakan Erdenal (ata-kaner)" 
 
 # Set build fingerprint / ID / Product Name etc.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m8qlul_htc_europe TARGET_DEVICE=htc_m8qlul
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="2.21.401.1 CL826429 release-keys"
+     PRODUCT_NAME=m8qlul_htc_europe \
+     TARGET_DEVICE=htc_m8qlul \
+     PRODUCT_NAME="HTC One M8s" \
+     PRODUCT_DEVICE="m8qlul" \
+     PRIVATE_BUILD_DESC="2.21.401.1 CL826429 release-keys" \
+     PRODUCT_GMS_CLIENTID_BASE := android-htc
 
 BUILD_FINGERPRINT := "htc/m8qlul_htc_europe/htc_m8qlul:6.0.1/MMB29M/826429.1:user/release-keys"
+
+PRODUCT_OVERRIDE_INFO := true
+PRODUCT_OVERRIDE_FINGERPRINT := htc/m8qlul_htc_europe/htc_m8qlul:6.0.1/MMB29M/826429.1:user/release-keys
