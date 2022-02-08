567b472dae59932f01c4127179d8e86146bc1aea
hpnightowl <iamhp2k@gmail.com>
2022-02-09 05:27:48 +0530

    m8qlul: drop audio.a2dp.default
    - building from here
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168

--- device.mk
+++ device.mk
@@ -82,7 +82,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
-    audio.a2dp.default \
     audio.primary.msm8916 \
     audio_amplifier.msm8916 \
     audio_policy.msm8916 \
