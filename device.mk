9c72a31c22ae1a8ff9c227a33b4c125c25d4f259
Olricccc <ata_kaner@outlook.com>
2023-03-17 10:36:17 +0300

    Switch to 1.1 power HAL

--- device.mk
+++ device.mk
@@ -340,9 +340,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    power.msm8916 \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.1-service-qti
 
 # Ramdisk
 PRODUCT_PACKAGES += \
