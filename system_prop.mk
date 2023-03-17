3b0483adff48e68deeceb82280e988ef7f8c4313
Olricccc <ata_kaner@outlook.com>
2023-03-17 10:13:51 +0300

    Import Stock Dalvik heap overrides

--- system_prop.mk
+++ system_prop.mk
@@ -52,6 +52,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.preview.size=1920x1080
 
+# Dalvik heap
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
+
 # Dexopt - limit to 4 cores
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.dex2oat_thread_count=4
