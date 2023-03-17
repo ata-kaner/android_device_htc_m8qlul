582de6c7292188f516d7d2dc749979c4912eab3d
Olricccc <ata_kaner@outlook.com>
2023-03-17 11:08:28 +0300

    Add audio HAL

--- device.mk
+++ device.mk
@@ -73,7 +73,9 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
     audio.a2dp.default \
     audio.primary.msm8916 \
     audio.r_submix.default \
