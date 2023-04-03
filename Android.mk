ef47202bc1f670e94e5f09cb51996a0737428445
Olricccc <ata_kaner@outlook.com>
2023-04-03 19:47:19 +0300

    Fix keymaster symlinks
    qseecomd was unavaliable to load keymaster file cuz they were never symlinked to its proper location. This should fix that

--- Android.mk
+++ Android.mk
@@ -24,12 +24,12 @@ include $(CLEAR_VARS)
 KM_IMAGES := \
     keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt
 
-KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(KM_IMAGES)))
+KM_SYMLINKS := $(addprefix /firmware/image/,$(notdir $(KM_IMAGES)))
 $(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Keymaster firmware link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /system/vendor/firmware/$(notdir $@) $@
+	$(hide) ln -sf /system/vendor/firmware/keymaster/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(KM_SYMLINKS)
 
