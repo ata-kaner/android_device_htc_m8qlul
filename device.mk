3ba519cc3c0f55ed68f800946aba44cfd120b949
Olricccc <ata_kaner@outlook.com>
2023-04-07 21:10:32 +0300

    m8qlul: Update device.mk for new Camera HAL

--- device.mk
+++ device.mk
@@ -126,8 +126,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy \
+    android.hardware.camera.provider@2.4-impl-m8qlul \
+    camera.device@1.0-impl-m8qlul \
     libmm-qcamera \
     libshim_camera \
     Snap 
