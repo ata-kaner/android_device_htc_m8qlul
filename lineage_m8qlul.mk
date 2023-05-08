302f9906cdbe384aa396d200691b27d5c327e645
Olricccc <ata_kaner@outlook.com>
2023-05-08 17:37:16 +0300

    m8qlul: Clear lineage_m8qlul.mk

--- lineage_m8qlul.mk
+++ lineage_m8qlul.mk
@@ -17,30 +17,23 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Lineage stuff.
+
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Inherit from m8qlul device
 $(call inherit-product, device/htc/m8qlul/device.mk)
 
-
-# Release name
 PRODUCT_RELEASE_NAME := m8qlul
 BOARD_VENDOR := htc
 
-# Device naming
-PRODUCT_NAME := lineage_m8qlul
 PRODUCT_DEVICE := m8qlul
+PRODUCT_NAME := lineage_m8qlul
 PRODUCT_BRAND := htc
-PRODUCT_MANUFACTURER := HTC
 PRODUCT_MODEL := HTC One M8s
+PRODUCT_MANUFACTURER := HTC
 
-# Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-PRODUCT_NAME=m8qlul_htc_europe \
-TARGET_DEVICE=htc_m8qlul
+    PRODUCT_NAME=m8qlul_htc_europe \
+    TARGET_DEVICE=htc_m8qlul
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="2.21.401.1 CL826429 release-keys"
