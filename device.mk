778d9d45e5d6f832572335a11bb5b6eb62f50d16
Olricccc <ata_kaner@outlook.com>
2023-05-18 00:34:31 +0300

    m8qlul: Fix dts eagle and add eagle patch

--- device.mk
+++ device.mk
@@ -84,6 +84,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.msm8916 \
     audio_amplifier.msm8916 \
+    audio_policy.msm8916 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
