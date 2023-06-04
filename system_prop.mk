c01d022bc4a1421a4d730a741ce48bd9454526c9
ata-kaner <ata_kaner@outlook.com>
2023-06-04 10:22:14 +0300

    m8qlul: kang display props from hiae

--- system_prop.mk
+++ system_prop.mk
@@ -79,11 +79,29 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.hwc.mdpcomp.enable=true \
-    ro.opengles.version=196608 \
-    ro.sf.lcd_density=420 \
-    debug.sf.disable_backpressure=1 \
-    debug.sf.latch_unsignaled=1
+debug.egl.hw=0 \
+debug.enable.sglscale=1 \
+debug.hwui.use_buffer_age=false \
+debug.mdpcomp.logs=0 \
+debug.sf.hw=0 \
+debug.sf.disable_backpressure=1 \
+debug.sf.recomputecrop=0 \
+debug.sf.latch_unsignaled=1 \
+dev.pm.dyn_samplingrate=1 \
+persist.demo.hdmirotationlock=false \
+persist.hwc.mdpcomp.enable=true \
+persist.hwc.downscale_threshold=1.15 \
+ro.opengles.version=196610 \
+ro.sf.lcd_density=420 \
+persist.sys.wfd.virtual=0 \
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+ro.surface_flinger.vsync_event_phase_offset_ns=6000000 \
+ro.surface_flinger.vsync_sf_event_phase_offset_ns=2000000
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
