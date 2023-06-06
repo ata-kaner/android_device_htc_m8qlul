819782a31713fa0e959f9e0fd6f81e12affc24df
ata-kaner <ata_kaner@outlook.com>
2023-06-06 13:03:03 +0300

    m8qlul: Use stock thermal-engine.conf

--- device.mk
+++ device.mk
@@ -342,7 +342,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf
+$(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
