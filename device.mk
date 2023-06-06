afa9da22f87609ce3aa9d3bbadea1f7ff5929ab0
ata-kaner <ata_kaner@outlook.com>
2023-06-06 13:02:44 +0300

    m8qlul: Switch to binderized gnss HAL

--- device.mk
+++ device.mk
@@ -191,6 +191,7 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service \
     libloc_eng \
     libloc_core \
     libgps.utils \
